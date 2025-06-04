cc261092afc6a1bc53ea72f1feb5b82768bf3c8f
Grkila <64324148+Grkila@users.noreply.github.com>
2025-06-04 15:06:13 +0200

    Delete build directory

--- build/husarion_ugv_controller/colcon_command_prefix_build.sh
+++
@@ -1,5 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/husarion_ws/install/husarion_ugv_utils/share/husarion_ugv_utils/package.sh"
-. "/husarion_ws/install/mecanum_drive_controller/share/mecanum_drive_controller/package.sh"
-. "/husarion_ws/install/ros_components_description/share/ros_components_description/package.sh"
-. "/husarion_ws/install/husarion_ugv_description/share/husarion_ugv_description/package.sh"
--- build/husarion_ugv_description/ament_cmake_environment_hooks/setup_envs.sh
+++
@@ -1,3 +0,0 @@
-ament_prepend_unique_value GAZEBO_MODEL_PATH "/husarion_ws/install/husarion_ugv_description/share"
-ament_prepend_unique_value GZ_SIM_RESOURCE_PATH "/husarion_ws/install/husarion_ugv_description/share"
-ament_prepend_unique_value IGN_GAZEBO_RESOURCE_PATH "/husarion_ws/install/husarion_ugv_description/share"
